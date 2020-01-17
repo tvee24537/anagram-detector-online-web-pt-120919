131662100bca1e18c35a94da57674b2feb12053e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-17 03:56:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(wordArray)
+      wordArray.select do |element|
+      (@word.split("").sort) == (element.split("").sort) #split a word into an array of letters and compare
+    end
+  end
+end
